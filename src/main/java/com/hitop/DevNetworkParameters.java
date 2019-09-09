098cabb00372f136fcf4107080d1a6a6a60e7cd1
nick avlo <avlo@yahoo.com>
2019-09-09 19:00:33 +0800

    mainnet/testnet refactor into components

--- src/main/java/com/hitop/DevNetworkParameters.java
+++ src/main/java/com/hitop/DevNetworkParameters.java
@@ -2,14 +2,18 @@ package com.hitop;
 
 import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.params.TestNet3Params;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 @Profile("dev")
 public class DevNetworkParameters implements com.hitop.NetworkParameters {
+  final Logger logger = LoggerFactory.getLogger(DevNetworkParameters.class);
   @Override
   public NetworkParameters getNetworkParameters() {
+    logger.info("using {} network.", TestNet3Params.ID_TESTNET);
     return TestNet3Params.get();
   }
 }
--- src/main/java/com/hitop/ProdNetworkParameters.java
+++ src/main/java/com/hitop/ProdNetworkParameters.java
@@ -2,14 +2,18 @@ package com.hitop;
 
 import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.params.MainNetParams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 @Profile("prod")
 public class ProdNetworkParameters implements com.hitop.NetworkParameters {
+  final Logger logger = LoggerFactory.getLogger(ProdNetworkParameters.class);
   @Override
   public NetworkParameters getNetworkParameters() {
+    logger.info("using {} network.", MainNetParams.ID_MAINNET);
     return MainNetParams.get();
   }
 }
