8a4f7295da8ad6adf26de97265f4abbbcce91b25
nick avlo <avlo@yahoo.com>
2020-06-03 02:14:08 -0700

    dynamic order and receipt page names

--- src/main/resources/static/js/sse.js
+++ src/main/resources/static/js/sse.js
@@ -1,9 +1,11 @@
 function registerSSEvent() {
   this.start = function () {
+    document.getElementById("pagestate").innerHTML = "Order";
     this.source = new EventSource("/receipt-sse");
     this.source.addEventListener("message", function (event) {
       $(".container-contact100-form-btn").hide();
       $(".contact50-form-title").hide();
+      document.getElementById("pagestate").innerHTML = "Receipt";
       document.getElementById("name").innerHTML = JSON.parse(event.data).name;
     });
     
