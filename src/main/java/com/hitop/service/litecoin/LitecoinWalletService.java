84ff4565df7c99fd2a9d6cd823647424eaed66e3
nick avlo <avlo@yahoo.com>
2020-07-27 22:32:21 -0700

    first pass

---
+++ src/main/java/com/hitop/service/litecoin/LitecoinWalletService.java
@@ -0,0 +1,124 @@
+package com.hitop.service.litecoin;
+
+/*
+ *  Copyright 2020 Nick Avlonitis
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */    
+
+import org.litecoinj.core.Address;
+import org.litecoinj.core.Coin;
+import org.litecoinj.core.InsufficientMoneyException;
+import org.litecoinj.core.LegacyAddress;
+import org.litecoinj.core.SegwitAddress;
+import org.litecoinj.core.Transaction;
+import org.litecoinj.core.TransactionOutput;
+import org.litecoinj.kits.WalletAppKit;
+import org.litecoinj.wallet.SendRequest;
+import org.litecoinj.wallet.listeners.WalletCoinsReceivedEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+import com.hitop.NetworkParameters;
+import com.hitop.service.WalletService;
+
+@Service
+@ConditionalOnProperty(
+    name = "spring.profiles.active", 
+    havingValue = "test")
+public class LitecoinWalletService implements WalletService {
+  private final static Logger log = LoggerFactory.getLogger(LitecoinWalletService.class);
+
+  private final NetworkParameters parameters;
+  private final WalletAppKit walletAppKit;
+
+  @Autowired
+  public LitecoinWalletService(
+      final NetworkParameters params,
+      final WalletAppKit walletAppKit) throws Exception {
+    this.parameters = params;
+    this.walletAppKit = walletAppKit;
+  }
+
+  @Override
+  public String getTxReceiveAddress(final Transaction tx) {
+    // TODO: replace for/if with functional functionalIF / lambda
+    for(TransactionOutput txo : tx.getOutputs()){
+      if (txo.isMine(walletAppKit.wallet())) {
+        String walletTxo = txo.getScriptPubKey().getToAddress(this.parameters.getNetworkParameters(), true).toString();
+        log.info("wallet txo: {}", walletTxo);
+        return walletTxo;
+      }
+    }
+    log.error("TXO not found in our wallet");
+    // TODO: needs exception handling/wrapper
+    return "****** REPLACE THIS STRING WITH \"TXO NOT IN OUR WALLET\" EXCEPTION ************";
+  }
+
+  @Override
+  public void addCoinsReceivedEventListener(final WalletCoinsReceivedEventListener listener) {
+    walletAppKit.wallet().addCoinsReceivedEventListener(listener);
+  }
+
+  @Override
+  public String getFreshSendToAddress() {
+    // TODO: issue w/ Segwit, replace when fixed
+    return LegacyAddress.fromKey(this.parameters.getNetworkParameters(), walletAppKit.wallet().freshReceiveKey()).toString();
+  }
+  
+  @Override
+  public Address getLegacySendToAddress(final String address) {
+    return LegacyAddress.fromString(this.parameters.getNetworkParameters(), address);
+  }
+  
+  // TODO: issue w/ Segwit, replace when fixed
+  public Address getSegwitSendToAddress(final String address) {
+    return SegwitAddress.fromString(this.parameters.getNetworkParameters(), address);
+  }
+
+  @Override
+  public String getBalance() {
+    return getCoinBalance().toFriendlyString();
+  }
+
+  @Override
+  public Coin getCoinBalance() {
+    walletAppKit.wallet().cleanup();
+    return walletAppKit.wallet().getBalance();
+  }
+  
+  @Override
+  public boolean sendMoney(final SendRequest req) {
+    try {
+      walletAppKit.wallet().sendCoins(req);
+      return true;
+    } catch (InsufficientMoneyException e) {
+      log.info("************************************************************************");
+      log.info("************* SHOULD NOT OCCUR SINGE WE'RE CHECKING FOR THIS ***********");
+      log.info("************************************************************************");
+      log.info("************************************************************************");
+      log.info("exception getMessage() {}", e.getMessage());
+      log.info("************************************************************************");
+      log.info("exception getLocalizedMessage() {}", e.getLocalizedMessage());
+      log.info("************************************************************************");
+      log.info("exception toFriendlyString() {}", e.missing.toFriendlyString());
+      log.info("************************************************************************");
+      return false;
+    }
+  }
+}
