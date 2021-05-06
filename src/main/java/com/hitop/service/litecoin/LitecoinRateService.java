17792561382ea042967801f3771adc0029fc278a
Nick Avlonitis <avlo@yahoo.com>
2021-05-05 19:54:56 -0700

    cleanup

--- src/main/java/com/hitop/service/litecoin/LitecoinRateService.java
+++ src/main/java/com/hitop/service/litecoin/LitecoinRateService.java
@@ -17,7 +17,7 @@ package com.hitop.service.litecoin;
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- */    
+ */
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
